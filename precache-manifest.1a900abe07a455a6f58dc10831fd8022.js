6b2ac8125d2048317d8b8a58bf4af37e325ef250
Maksym9669 <sychov@mail.usf.edu>
2019-04-17 17:35:15 -0400

    Updates

---
+++ precache-manifest.1a900abe07a455a6f58dc10831fd8022.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4c81ca1677d882eab21",
+    "url": "/myWeatherApp/static/css/main.9f866ca8.chunk.css"
+  },
+  {
+    "revision": "c4c81ca1677d882eab21",
+    "url": "/myWeatherApp/static/js/main.bd9d00cb.chunk.js"
+  },
+  {
+    "revision": "e418136278202b7096a4",
+    "url": "/myWeatherApp/static/js/runtime~main.acc04077.js"
+  },
+  {
+    "revision": "4bc456b18c70742b1ab0",
+    "url": "/myWeatherApp/static/js/2.cc6a32ff.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myWeatherApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f25d60da25d37343f092cd0a6bf9e381",
+    "url": "/myWeatherApp/index.html"
+  }
+];
